a90f04c1b60525ec4286f4580d4dd48ac0f8aa82
Stephen L <67349718+StephenL13@users.noreply.github.com>
2022-02-01 07:29:54 +0000

    index.js fix

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 const fs = require('fs')
-require('dotenv').config();
-require('http').createServer((req, res) => res.end('The bot is alive!')).listen(3000)
-client.login(process.env.TOKEN);
-// lines of code required above for the bot's core
-
 const intents = new Discord.Intents(32767);
 const client = new Discord.Client({ intents });
+client.login(process.env.TOKEN);
+require('dotenv').config();
+require('http').createServer((req, res) => res.end('The bot is alive!')).listen(3000)
+// lines of code required above for the bot's core
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
