131dc1547c8684006b7e33a85fa3c67d3b34a3f5
StephenL13 <luciajastephen@gmail.com>
2022-05-16 00:19:47 +0800

    ok

--- commands/moderation/closejail.js
+++ commands/moderation/closejail.js
@@ -1,18 +1,21 @@
 module.exports.run = async(client, message, args, prefix) => {
     const moderatorrole = message.guild.roles.cache.get("812126006344548412");
 
-    if (!moderatorrole) return message.delete().then(async () => {
+    if(!moderatorrole) return message.delete().then(async () => {
         await message.author.send("You're not a staff member authorized to use this command.");
         }).catch((e) => {});
     if(!message.member.permissions.has("KICK_MEMBERS")) return message.delete().then(async () => {
         await message.author.send("You're not a staff member authorized to use this command.");
         }).catch((e) => {});
-    if(!message.channel.parent.id === "938636987504685086") return console.log(`${message.author.tag} executed in a non-jail ticket.`)
-    message.react("🔓")
-    message.channel.send("**The channel will be closed in five seconds.**")
-    setTimeout(() => {
-        message.channel.delete()
-    }, 5000)
+    if(!message.channel.parent.id === "938636987504685086") {
+        return console.log(`${message.author.tag} executed in a non-jail ticket.`)
+    } else {
+        message.react("🔓")
+        message.channel.send("**The channel will be closed in five seconds.**")
+        setTimeout(() => {
+            message.channel.delete()
+        }, 5000)
+    }
 }
 
 module.exports.help = {
